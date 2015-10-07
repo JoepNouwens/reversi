6832660eb184247a68731102ecc4f038e3393649
tennapel <rtennapel@hotmail.com>
2015-10-07 17:44:22 +0200

    Klassen opgedeelt
    Nieuwe file voor Veld-klasse

--- Reversi/Reversi/Reversi.cs
+++ Reversi/Reversi/Reversi.cs
@@ -11,7 +11,6 @@ class ReversiForm : Form
     {
         int veldomvang;
         //Variabelen om gemakkelijk omvang van het veld aan te passen
-        // gerben heeft iets aangepast
         breedte = 6;
         hoogte = 6;
         veldomvang = 80;
@@ -55,48 +54,6 @@ class ReversiForm : Form
     void ReversiForm_Paint(object o, PaintEventArgs pea)
     {
 
-    }
-}
-
-class Veld : Panel
-{
-    int toestand;
-    bool legaal;
-    object parent;
-    int x, y, omvang;
-
-    public Veld(object o, int xpos, int ypos, int veldomvang)
-    {
-        parent = o;
-        x = xpos;
-        y = ypos;
-        omvang = veldomvang;
-
-        this.Size = new Size(omvang, omvang);
-        this.Paint += Veld_Paint;
-        this.MouseClick += Veld_Play;
-    }
-
-    public void Veld_Paint(object o, PaintEventArgs pea)
-    {
-        //Functie die het tekenen van het veld moet gaan handelen
-        //(Dus: checken wat de staat is, e.d., vervolgens juiste staat tekenen)
-        Graphics g;
-        Rectangle veld;
-        g = pea.Graphics;
-
-        veld = new Rectangle(0, 0, omvang, omvang);
-        if ((x % 2 + y % 2) % 2 == 0)
-            g.FillRectangle(Brushes.LightBlue, veld);
-        else
-            g.FillRectangle(Brushes.LightCoral, veld);
-
-    }
-
-    public void Veld_Play(object o, MouseEventArgs mea)
-    {
-        //Functie die moet gaan checken: is de zet legaal (voor de huidige speler),
-        //zo ja, speel hem uit (en draai alle gevangen stenen om)
     }
 }
 
---
+++ Reversi/Reversi/Veld.cs
@@ -0,0 +1,41 @@
+﻿class Veld : Panel
+{
+    int toestand;
+    bool legaal;
+    object parent;
+    int x, y, omvang;
+
+    public Veld(object o, int xpos, int ypos, int veldomvang)
+    {
+        parent = o;
+        x = xpos;
+        y = ypos;
+        omvang = veldomvang;
+
+        this.Size = new Size(omvang, omvang);
+        this.Paint += Veld_Paint;
+        this.MouseClick += Veld_Play;
+    }
+
+    public void Veld_Paint(object o, PaintEventArgs pea)
+    {
+        //Functie die het tekenen van het veld moet gaan handelen
+        //(Dus: checken wat de staat is, e.d., vervolgens juiste staat tekenen)
+        Graphics g;
+        Rectangle veld;
+        g = pea.Graphics;
+
+        veld = new Rectangle(0, 0, omvang, omvang);
+        if ((x % 2 + y % 2) % 2 == 0)
+            g.FillRectangle(Brushes.LightBlue, veld);
+        else
+            g.FillRectangle(Brushes.LightCoral, veld);
+
+    }
+
+    public void Veld_Play(object o, MouseEventArgs mea)
+    {
+        //Functie die moet gaan checken: is de zet legaal (voor de huidige speler),
+        //zo ja, speel hem uit (en draai alle gevangen stenen om)
+    }
+}
